4a33f7d42d4169038e9e2fdd709e9141f42b77de
EkaterinaKononenko <kaktysik4@ukr.net>
2022-08-17 08:02:45 +0300

    update main.js

--- cross-zero/js/main.js
+++ cross-zero/js/main.js
@@ -20,73 +20,73 @@ let winIndex = [
   [3, 5, 7],
 ];
 
-for (let i = 1; i <= 9; i+=1) {
-  area.innerHTML += "div class='cell' pos=" + i + "></div>";
+for (let i = 1; i <= 9; i += 1) {
+  area.innerHTML += "<div class='cell' pos=" + i + "></div>";
 }
 
-for (let i = 0; i < cell.length; i+=1) {
-  cell[i].addEventListener('click', cellClick, false);
+for (let i = 0; i < cell.length; i += 1) {
+  cell[i].addEventListener("click", cellClick, false);
 }
 
 function cellClick() {
-    let data = [];
+  let data = [];
 
-    if (!this.innerHTML) {
-        this.innerHTML = player;
-    } else {
-        alert("Ну как можно не заметить? Занята ячейка!");
-        return;
-    }
-
-    for (let i in cell) {
-        if (cell[i].innerHTML == player) {
-            data.push(parseInt(cell[i].getAttribute('pos')));
-        }
-    }
-    if (checkWin(data)) {
-        stat[player] += 1;
-        restart("Внимание! Апплодисменты в знак победы: " + player);
-    } else {
-        let draw = true;
-        for (let i in cell)
-    } if (cell[i].innerHTML == '') draw = false;
-
-
-        if (draw) {
-    stat.d += 1;
-    restart("Победила дружба!!!");
+  if (!this.innerHTML) {
+    this.innerHTML = player;
+  } else {
+    alert("Ячейка занята!");
+    return;
   }
 
+  for (let i in cell) {
+    if (cell[i].innerHTML == player) {
+      data.push(parseInt(cell[i].getAttribute("pos")));
+    }
+  }
+  if (checkWin(data)) {
+    stat[player] += 1;
+    restart("Внимание! Апплодисменты в знак победы: " + player);
+  } else {
+    let draw = true;
+    for (let i in cell) {
+      if (cell[i].innerHTML == "") draw = false;
+    }
 
-player = player == "X" ? "O" : "X";
-currentPlayer.innerHTML = player.toUpperCase();}
+    if (draw) {
+      stat.d += 1;
+      restart("Победила дружба!!!");
+    }
+  }
 
+  player = player == "X" ? "O" : "X";
+  currentPlayer.innerHTML = player.toUpperCase();
+}
 
 function checkWin(data) {
-    for (let i in winIndex) {
-        let win = true;
-        for (let j in winIndex[i]) {
-            let id = winIndex[i][j];
-            let ind = data.indexOf(id);
+  for (let i in winIndex) {
+    let win = true;
+    for (let j in winIndex[i]) {
+      let id = winIndex[i][j];
+      let ind = data.indexOf(id);
 
-            if (ind == -1) {
-                win = false
-            }
-        }
-        if (win) return true;
+      if (ind == -1) {
+        win = false;
+      }
     }
-    return false;
+    if (win) return true;
+  }
+  return false;
 }
 
 function restart(text) {
-    alert(text);
-    for (let i = 0; i < cell.length; i+=1) {
-        cell[i].innerHTML = '';
-    }
-    updateStat();
+  alert(text);
+  for (let i = 0; i < cell.length; i += 1) {
+    cell[i].innerHTML = "";
+  }
+  updateStat();
 }
 function updateStat() {
-    document.getElementById('sX').innerHTML = stat.X;
-    document.getElementById('sO').innerHTML = stat.o;
-    document.getElementById('sD').innerHTML = stat.d;
+  document.getElementById("sX").innerHTML = stat.X;
+  document.getElementById("sO").innerHTML = stat.O;
+  document.getElementById("sD").innerHTML = stat.d;
 }
